c73ad2b7774d903d260d41ae04169185853c0dfe
matusporuben <matus.poruben@gmail.com>
2024-10-19 16:58:47 +0200

    update nightly script

--- cli/nightly.mjs
+++ cli/nightly.mjs
@@ -1,12 +1,13 @@
 import {readFile, writeFile} from 'fs/promises';
-
+import {exec} from 'child_process';
 
 const main = async () => {
   const packageJSON = JSON.parse(await readFile('package.json', 'utf-8'))
-  console.log(packageJSON.version)
-  packageJSON.version =  packageJSON.version + '-' + Date.now()
-
-  await writeFile('package.json', JSON.stringify(packageJSON, null, 2))
+  exec('git rev-parse HEAD', async (err, stdout, stderr) => {
+    const commitHash = stdout.split('\n')[0];
+    packageJSON.version =  packageJSON.version + '-' + commitHash
+    await writeFile('package.json', JSON.stringify(packageJSON, null, 2))
+  })
 }
 
 main()
